00a4438b8dd4c462f72d08121de9d4db9b5f6867
FurkanPehlvn <pehlivanfurkan93@gmail.com>
2024-05-20 23:33:53 +0300

    nav bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import "./headercss.css";
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
+  const location = useLocation();
+
+  const isMainPage = location.pathname === "/";
+
   return (
     <div className="header-content">
-      <img className="header-content-img" src={logo} alt={logo}></img>
+      <img className="header-content-img" src={logo} alt="Logo" />
+      {!isMainPage && (
+        <nav className="header-nav">
+          <Link className="nav-link" to="/">
+            Anasayfa
+          </Link>
+          <Link className="nav-link" to="/orderpage">
+            Sipariş oluştur
+          </Link>
+        </nav>
+      )}
     </div>
   );
 };
